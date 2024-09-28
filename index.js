dc73f7156d2dae02b9dfc7e639705b535b86f3af
azka5123 <115395179+azka5123@users.noreply.github.com>
2024-09-28 20:16:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "express-test",
+    database: "express_test",
 });
 
 db.connect((err) => {
